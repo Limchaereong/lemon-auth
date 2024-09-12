d098d81e53fb66f55fbb980bbf44baa51dc2c67d
Limchaereong <sa990422@naver.com>
2024-09-12 12:43:11 +0900

    fix : JWT_SECRET 인식 문제 테스트

--- src/test/java/org/example/authserver/AuthServerApplicationTests.java
+++ src/test/java/org/example/authserver/AuthServerApplicationTests.java
@@ -3,13 +3,14 @@ package org.example.authserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {"JWT_SECRET=testSecretKeyForJwt"})
 class AuthServerApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
