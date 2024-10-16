c3e43bb4376507fe30284fa3208a0c70d2e272d5
Limchaereong <sa990422@naver.com>
2024-10-16 17:27:41 +0900

    refactor: 패키지 이름 변경

--- src/main/java/org/basoup/authserver/common/config/SecurityConfig.java
+++ src/main/java/org/basoup/authserver/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.basoup.authserver.config;
+package org.basoup.authserver.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
